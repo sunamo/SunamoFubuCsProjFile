267d2477f231ddfaf665d85a991c79f5aaaa701f
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-20 06:51:20 +0200

    Before trying call automatic to fix remain errors

--- _NotMine/None.cs
+++ _NotMine/None.cs
@@ -1,12 +1,20 @@
-namespace SunamoFubuCsProjFile;
-
+namespace
+#if SunamoGoogleSheets
+SunamoGoogleSheets
+#elif SunamoString
+SunamoString
+#elif SunamoStringShared
+SunamoStringShared
+#else
+SunamoFubuCsProjFile
+#endif
+;
 public class None : Content
 {
     public None()
     {
         Name = "None";
     }
-
     public None(string include) : base("None", include)
     {
     }
