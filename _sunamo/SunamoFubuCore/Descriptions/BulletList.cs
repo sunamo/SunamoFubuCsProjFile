02fc307b5593542e7592841a65598b90daf9139c
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-21 20:36:30 +0200

    Everyone on nuget

---
+++ _sunamo/SunamoFubuCore/Descriptions/BulletList.cs
@@ -0,0 +1,20 @@
+namespace SunamoFubuCsProjFile;
+
+
+public class BulletList
+{
+    public BulletList()
+    {
+        Children = new List<Description>();
+    }
+    public IList<Description> Children { get; }
+    public string Name { get; set; }
+    public string Label { get; set; }
+    public bool IsOrderDependent { get; set; }
+    public void AcceptVisitor(IDescriptionVisitor visitor)
+    {
+        visitor.StartList(this);
+        Children.Each(x => x.AcceptVisitor(visitor));
+        visitor.EndList();
+    }
+}
